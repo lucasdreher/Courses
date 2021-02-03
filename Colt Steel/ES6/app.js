a357d6f6355237f1b1abbefa7da7c34119f1d9b6
Lucas Dreher <lucasdreher@gmail.com>
2021-02-03 21:01:44 +0100

    Colt ES6 basic files

---
+++ Colt Steel/ES6/app.js
@@ -0,0 +1,11 @@
+/* The Web Developer Bootcamp 2021
+Instructor
+Colt Steele
+Developer and Bootcamp Instructor */
+
+// console.log('Connected!');
+
+// const password = prompt('please enter a new password');
+
+//Password must be 6+ characters
+//Password cannot include space
