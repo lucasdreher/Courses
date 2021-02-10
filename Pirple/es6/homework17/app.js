0312b33ed19084bd9b9c274821b0e65d2237b1e6
Lucas Dreher <lucasdreher@gmail.com>
2021-02-10 14:35:00 +0100

    HW 17 css complete

--- Pirple/es6/homework17/app.js
+++ Pirple/es6/homework17/app.js
@@ -1,1 +1,26 @@
-console.log('connected');
+// Check if
+function notifyMe() {
+	const title = 'NOTIFICATION ALARM CONNECTED',
+		msg = 'Hey! Your notification alarm is up and running.';
+	if (!('Notification' in window)) {
+		alert('This browser does not support system notifications');
+	} else if (Notification.permission === 'granted') {
+		notify(title, msg);
+	} else if (Notification.permission !== 'denied') {
+		Notification.requestPermission(function(permission) {
+			if (permission === 'granted') {
+				notify(title, msg);
+			}
+		});
+	}
+}
+
+notifyMe();
+
+function notify(title, msg) {
+	let notification = new Notification(title, {
+		icon: 'https://img.icons8.com/dusk/64/000000/alarm.png',
+		body: msg
+	});
+	setTimeout(notification.close.bind(notification), 10000);
+}
