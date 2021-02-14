28e9e3d4667f15670d3697eeafbf7dbcd845f5c6
Lucas Dreher <lucasdreher@gmail.com>
2021-02-14 07:40:18 +0100

    Babel course completed

--- Pirple/es6/babel/app.js
+++
@@ -1,6 +0,0 @@
-const myDid = document.getElementById('main');
-const myArr = [ 1, 2, 3, 4, 5 ];
-for (const prop of myArr) {
-	console.log(prop);
-	myDid.innerHTML += `<p>${prop}</p>`;
-}
---
+++ Pirple/es6/babel/src/app.js
@@ -0,0 +1,22 @@
+const myDid = document.getElementById('main');
+const myArr = [ 1, 2, 3, 4, 5 ];
+for (const prop of myArr) {
+	myDid.innerHTML += `<p>${prop}</p>`;
+}
+
+class Person {
+	constructor(name, age) {
+		this.name = name;
+		this.age = age;
+	}
+	sayHi() {
+		console.log(`Hi, ${this.name}. You have ${this.age} years of age.`);
+	}
+}
+
+const billy = new Person('Billy', 21);
+billy.sayHi();
+const sally = new Person('Sally', 16);
+sally.sayHi();
+
+console.log('Watched!');
